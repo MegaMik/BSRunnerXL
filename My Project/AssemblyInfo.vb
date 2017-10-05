c8052262a770605a8ec008db8fdfe68d018df06d
Mik Wadstrom <csi-watch@live.se>
2017-10-05 09:06:05 +0200

    Add files via upload

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Resources
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Bayshore Runner")> 
+<Assembly: AssemblyDescription("Bayshore Runner")> 
+<Assembly: AssemblyCompany("Bayshore Ocean Co.,Ltd.")> 
+<Assembly: AssemblyProduct("Bayshore Runner")> 
+<Assembly: AssemblyCopyright("Copyright © 2012 Mik Wadström")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("5bfda1ef-eca1-47ec-bcfe-08b1f6dbacec")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("0.9.5.9")> 
+<Assembly: AssemblyFileVersion("0.15.1.9")> 
+
+<Assembly: NeutralResourcesLanguageAttribute("en")> 
